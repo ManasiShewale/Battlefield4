465bd8865d6bbcb872791ca6edaa3cf004f42e5a
manasishewale <manasishewale9@gmail.com>
2023-12-26 16:26:00 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const fetchData = require("./db");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
